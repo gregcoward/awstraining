f11f104a698032650ac2b464fde50438824dc584
tstanley93 <t.stanley@f5.com>
2017-03-08 17:16:15 -0800

    new calico stuff

--- cc_deploy.sh
+++ cc_deploy.sh
@@ -1,34 +1,30 @@
-#!/bin/bash -xe
-
-# Create variables
-masterIP="empty"
-node1IP="empty"
-node2IP="empty"
-
+#!/bin/bash -x
+function pushFiles() {
+    scp -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/k8s-bigip-ctlr-master-26326.tar.gz admin@${1}:~/k8s-bigip-ctlr-master-26326.tar.gz
+    scp -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/calicoctl admin@${1}:~/calicoctl
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "sudo docker load -i ~/k8s-bigip-ctlr-master-26326.tar.gz"
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2"
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "sudo docker images"
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "sudo docker tag 93d8e97443f8 localhost:5000/cc"
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "sudo docker push localhost:5000/cc"
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "chmod +x ~/calicoctl"
+    ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${1} "sudo ~/calicoctl /usr/bin"
+}
 # Get Master IP address
 masterIP=$( kubectl get ep | awk '/^kubernetes / { print $2 }' | cut -d: -f1 )
-
 # Find Node IP addresses
 kubectl get nodes | grep internal | cut -d" " -f1 | while read line
 do
    ipAddress=$(nslookup $line | awk '/^Address: / { print $2 }')
    if [[ ${ipAddress} == ${masterIP} ]]; then
         echo "Do nothing."
-   elif [[ ${node1IP} == "empty" ]]; then
-        scp -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/k8s-bigip-ctlr-master-26326.tar.gz admin@${ipAddress}:~/k8s-bigip-ctlr-master-26326.tar.gz
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker load -i ~/k8s-bigip-ctlr-master-26326.tar.gz"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker images"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker tag 93d8e97443f8 localhost:5000/cc"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker push localhost:5000/cc"
    else
-        scp -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/k8s-bigip-ctlr-master-26326.tar.gz admin@${ipAddress}:~/k8s-bigip-ctlr-master-26326.tar.gz
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker load -i ~/k8s-bigip-ctlr-master-26326.tar.gz"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker images"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker tag 93d8e97443f8 localhost:5000/cc"
-        ssh -i /home/centos/privatekey.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@${ipAddress} "sudo docker push localhost:5000/cc" 
+        echo ${ipAddress} >> /tmp/nodes
    fi
 done
-
-exit
+#
+exec 3</tmp/nodes
+while IFS='' read -r -u 3 line || [[ -n "$line" ]] 
+do
+   pushFiles ${line}
+done
