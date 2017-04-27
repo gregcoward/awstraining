fccf22bc558a3c9acf29aa8cc61784f561e02868
tstanley93 <t.stanley@f5.com>
2017-04-27 16:19:55 -0700

    fixed order of do while

--- dbinstance.ps1
+++ dbinstance.ps1
@@ -13,22 +13,20 @@ New-LocalUser -Name $username -Password $pass -PasswordNeverExpires
 Add-LocalGroupMember -Group \"Administrators\" -Member $username
 
 #region Functions
-function instTools ($username) {
+function instTools ([string]$username) {
     Invoke-WebRequest "https://raw.githubusercontent.com/gregcoward/awstraining/master/MsSqlCmdLnUtils.msi" -OutFile "c:\users\$username\downloads\MsSqlCmdLnUtils.msi"
     Invoke-WebRequest "https://raw.githubusercontent.com/gregcoward/awstraining/master/msodbcsql.msi" -OutFile "C:\Users\$username\downloads\msodbcsql.msi"
     msiexec.exe /i "C:\Users\$username\downloads\msodbcsql.msi" /qn IACCEPTMSODBCSQLLICENSETERMS=YES
     msiexec /i "c:\users\$username\downloads\MsSqlCmdLnUtils.msi" /qn IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES
 }
 
-function createDb ($username) {
+function createDb ([string]$username) {
     & 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\SQLCMD.EXE' -E -S localhost -Q "CREATE LOGIN [$env:COMPUTERNAME\$username] FROM WINDOWS WITH DEFAULT_DATABASE=[master]"
     & 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\SQLCMD.EXE' -E -S localhost -Q "EXEC [sys].[sp_addsrvrolemember] @loginame = N'$env:COMPUTERNAME\$username', @rolename = N'sysadmin'"
     & 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\SQLCMD.EXE' -E -S localhost -Q "CREATE DATABASE nopcomm"
 }
 
-function Unzip {
-    param([string]$zipfile, [string]$outpath)
-
+function Unzip ([string]$zipfile, [string]$outpath) {
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 #endregion
@@ -51,14 +49,14 @@ else {
     do {
         $sqlExist = $(& 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\SQLCMD.EXE' -S tcp:$dbserver,1433 -Q "IF DB_ID('nopcomm') IS NOT NULL BEGIN PRINT 'Database Exists' END")
         if (!$sqlExist) {
+            Write-Host "Sleeping for 10 Seconds"
+            Start-Sleep -Seconds 10
+        }
+        else {
             ## Download the file:
             Invoke-WebRequest https://raw.githubusercontent.com/gregcoward/awstraining/master/nopCommerce_3.90_NoSource.zip -OutFile c:\users\$username\downloads\nopCommerce_3.90_NoSource.zip
             Unzip "c:\users\$username\downloads\nopCommerce_3.90_NoSource.zip" "C:\inetpub\wwwroot\"
-            $i = 1
-        }
-        else {
-            Write-Host "Sleeping for 10 Seconds"
-            Start-Sleep -Seconds 10
+            $i = 1        
         }
     } while ($i -eq 0)
 }
